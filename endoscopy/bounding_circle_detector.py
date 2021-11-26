10e9581fd72aa4aa5d48ec167a2deda34743f0b3
mhubii <m.huber_1994@hotmail.de>
2021-11-26 12:26:37 +0000

    added batching support

--- endoscopy/bounding_circle_detector.py
+++ endoscopy/bounding_circle_detector.py
@@ -16,25 +16,33 @@ class BoundingCircleDetector():
         self.model = load_model(device, name)
         self.canny = kornia.filters.Canny()
 
-    def __call__(self, img: torch.FloatTensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def __call__(self, img: torch.FloatTensor, N: int=100) -> Tuple[torch.Tensor, torch.Tensor]:
         """Foward pass of BoundingCircleDetector.
 
         Args:
             img (torch.FloatTensor): Needs to be normalized in [0, 1].
+            N (int): Number of non-zero samples.
         Return:
-            center (torch.Tensor): Circle's center.
-            radius (torch.Tensor): Circle's radius.
+            center (torch.Tensor): Circle's center of shape Bx2.
+            radius (torch.Tensor): Circle's radius of shape B.
         """
         seg = self.model(img.to(self.device))
         _, edg = self.canny(seg)
 
-        pts = edg.nonzero().float()
+        pts = []
+        for e in edg:
+            if e.numel() < N:
+                raise RuntimeError("BoundingCircleDetector: Non suffiecient non-zero elements to sample from, got {}, required {}".format(e.numel(), N))
+            nonzero = e.nonzero().float()
+            pts.append(
+                nonzero[torch.randperm(nonzero.shape[0], device=self.device)[:N]]  # sampling without replacement: https://discuss.pytorch.org/t/torch-equivalent-of-numpy-random-choice/16146/19
+            )
+
+        pts = torch.stack(pts)
 
         A, b = circle_linear_system(pts)
         x = torch.linalg.lstsq(A, b).solution
 
-        center, radius = x[:2], const2rad(x)
+        center, radius = x[:,:2], const2rad(x)
 
-        return center, radius
-
-
+        return center.squeeze(), radius.squeeze()
--- endoscopy/utils/circle_linear_system.py
+++ endoscopy/utils/circle_linear_system.py
@@ -6,23 +6,23 @@ def circle_linear_system(pts: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]
     """Build a linear system that represents a circle.
 
     Args:
-        pts (torch.Tensor): N points of shape NxM, where M >= 2.
+        pts (torch.Tensor): BxN points of shape BxNxM, where M >= 2.
     Return:
         A (torch.Tensor): Linear system matrix.
         b (torch.Tensor): Offset to linear equation.
     """
     A = torch.stack(
         [
-            2*pts[:,-2],
-            2*pts[:,-1],
-            torch.ones_like(pts[:,-1])
-        ], axis=1
+            2*pts[...,-2],
+            2*pts[...,-1],
+            torch.ones_like(pts[...,-1])
+        ], axis=-1
     )
 
     b = torch.stack(
         [
-            torch.square(pts[:,-2]) + torch.square(pts[:,-1])
-        ], axis=1
+            torch.square(pts[...,-2]) + torch.square(pts[...,-1])
+        ], axis=-1
     )
 
     return A, b
@@ -31,9 +31,9 @@ def const2rad(x: torch.Tensor) -> torch.Tensor:
     """Retrieve radius from solution to linear system.
 
     Args:
-        x (torch.Tensor): Solution to circle linear system.
+        x (torch.Tensor): Solutions to circle linear system.
     Return:
         radius (torch.Tensor): Circle's radius.
     """
-    radius = torch.sqrt(x[2] + x[0]**2 + x[1]**2)
+    radius = torch.sqrt(x[:,2] + x[:,0]**2 + x[:,1]**2)
     return radius
