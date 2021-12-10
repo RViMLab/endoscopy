53e1349eb0faf9cc9cfb3bce0e4f7cf2759e3b03
Martin Huber <mhubii@users.noreply.github.com>
2021-12-10 13:18:27 +0000

    Update loader.py

--- endoscopy/utils/loader.py
+++ endoscopy/utils/loader.py
@@ -1,9 +1,8 @@
 import torch
 
 model_urls = {
-    "segmentation_unet_efficient_net_b4": "https://github.com/RViMLab/endoscopy/releases/download/0.0.1/segmentation_unet_efficient_net_b4.ckpt",
-    "segmentation_unet_resnet_34": "https://github.com/RViMLab/endoscopy/releases/download/0.0.1/segmentation_unet_resnet_34.ckpt",
-    "segmentation_unet_resnet_34_tiny": "https://github.com/RViMLab/endoscopy/releases/download/0.0.1/segmentation_unet_resnet_34_tiny.ckpt"
+    "seg_unet_resnet_34": "https://github.com/RViMLab/endoscopy/releases/download/0.0.1/seg_unet_resnet_34.pt",
+    "seg_unet_resnet_34_tiny": "https://github.com/RViMLab/endoscopy/releases/download/0.0.1/seg_unet_resnet_34_tiny.pt"
 }
 
 def load_model(device: str="cuda", name: str="segmentation_unet_resnet_34_tiny"):
