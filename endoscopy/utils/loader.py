98338b599debbd47f6b4805f101570ae7f1aa760
Martin Huber <mhubii@users.noreply.github.com>
2021-12-10 13:18:35 +0000

    Update loader.py

--- endoscopy/utils/loader.py
+++ endoscopy/utils/loader.py
@@ -5,6 +5,6 @@ model_urls = {
     "seg_unet_resnet_34_tiny": "https://github.com/RViMLab/endoscopy/releases/download/0.0.1/seg_unet_resnet_34_tiny.pt"
 }
 
-def load_model(device: str="cuda", name: str="segmentation_unet_resnet_34_tiny"):
+def load_model(device: str="cuda", name: str="seg_unet_resnet_34_tiny"):
     model = torch.hub.load_state_dict_from_url(model_urls[name])
     return model.eval().to(device)
