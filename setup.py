706ceb18d093ae87977549a16ee03413f983e77e
mhubii <m.huber_1994@hotmail.de>
2021-12-13 09:54:15 +0000

    fixed element checking

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setuptools.setup(
     name="endoscopy",
-    version="0.0.13",
+    version="0.0.14",
     author="Martin Huber",
     author_email="martin.huber@kcl.ac.uk",
     description="Image processing utilities for endoscopic images.",
