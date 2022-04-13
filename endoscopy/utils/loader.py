27a7471037da677e348e3d6f213073c06cf3b7ad
mhubii <m.huber_1994@hotmail.de>
2022-04-13 16:14:02 +0100

    updated links

--- endoscopy/utils/loader.py
+++ endoscopy/utils/loader.py
@@ -8,7 +8,8 @@ class MODEL(object):
     URLs to models are to include a 'cpu' or 'cuda' tag, respectively.
     """
     class HOMOGRAPHY_ESTIMATION_ENUM(Enum):
-        RESNET_34 = "https://github.com/RViMLab/endoscopy/releases/download/0.1.1/h_est_resnet_34_{}.pt"
+        H_48_RESNET_34 = "https://github.com/RViMLab/endoscopy/releases/download/0.1.1/h_est_48_resnet_34_{}.pt"
+        H_64_RESNET_34 = "https://github.com/RViMLab/endoscopy/releases/download/0.1.1/h_est_64_resnet_34_{}.pt"
 
     class SEGMENTATION_ENUM(Enum):
         UNET_RESNET_34 = "https://github.com/RViMLab/endoscopy/releases/download/0.1.1/seg_unet_resnet_34_{}.pt"
