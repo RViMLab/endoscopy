597ad7b559729cf5321eb4229432acb9cb912b88
mhubii <m.huber_1994@hotmail.de>
2023-01-20 12:55:31 +0000

    added predictor

--- endoscopy/__init__.py
+++ endoscopy/__init__.py
@@ -1,9 +1,11 @@
 from .bounding_circle_detector import BoundingCircleDetector
 from .homography_estimator import HomographyEstimator
+from .homography_predictor import HomographyPredictor
 from .utils import *
 
 __all__ = [
     "utils",
     "BoundingCircleDetector",
-    "HomographyEstimator"
+    "HomographyEstimator",
+    "HomographyPredictor",
 ]
