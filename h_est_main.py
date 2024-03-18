d393e3bf9019ee4c9a64b4f41470833163febc1d
mhubii <m.huber_1994@hotmail.de>
2024-03-18 08:54:46 +0000

    use different images in demo homography estimate #2

--- h_est_main.py
+++ h_est_main.py
@@ -7,20 +7,26 @@ from endoscopy.homography_estimator import HomographyEstimator
 from endoscopy.utils import MODEL, yt_alpha_blend
 
 if __name__ == "__main__":
+    # load model
     device = "cuda"
-
     homography_estimator = HomographyEstimator(
         model=MODEL.HOMOGRAPHY_ESTIMATION.H_48_RESNET_34, device=device
     )
-    img = np.load("data/laparoscopic_view.npy")
+
+    # load data
+    img = np.load("data/vid_0/frame_0.npy")
+    wrp = np.load("data/vid_0/frame_9.npy")
+
+    # convert to tensor and normalize
     img = image_to_tensor(img, keepdim=False).float() / 255.0
+    wrp = image_to_tensor(wrp, keepdim=False).float() / 255.0
 
     # estimate homography
-    h, duv = homography_estimator(img, img)
+    h, duv = homography_estimator(img, wrp)
 
     # visualize through blend
-    wrp = warp_perspective(img, h.inverse(), img.shape[-2:])
-    blend = yt_alpha_blend(img, wrp)
+    wrp_est = warp_perspective(img, h.inverse(), img.shape[-2:])
+    blend = yt_alpha_blend(wrp_est, wrp)
 
     blend = tensor_to_image(blend.cpu(), keepdim=False)
     cv2.imshow("blend", blend)
